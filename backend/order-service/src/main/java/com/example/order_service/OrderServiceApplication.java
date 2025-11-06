2969064600f8208074c66e691421f8a8b5ec7a82
Hưng Phan <phanphuochung.07022004@gmail.com>
2025-11-06 22:47:40 +0700

    initial commit

---
+++ backend/order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -0,0 +1,34 @@
+package com.example.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+
+		loadEnvFile();
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+	private static void loadEnvFile() {
+		try {
+			File envFile = new File("../.env");
+			if (envFile.exists()) {
+				Properties props = new Properties();
+				props.load(new FileInputStream(envFile));
+
+				props.forEach((key, value) -> {
+					System.setProperty(key.toString(), value.toString());
+				});
+
+				System.out.println("Loaded " + props.size() + " properties from .env file");
+			}
+		} catch (IOException e) {
+			System.err.println("Could not load .env file: " + e.getMessage());
+		}
+	}
+}
