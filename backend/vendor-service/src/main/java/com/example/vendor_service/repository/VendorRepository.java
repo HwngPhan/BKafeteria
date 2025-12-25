3cedb6537a4c1f41eaac584d2957b3baa933045a
Quan Nguyen <minhq6430@gmail.com>
2025-12-25 18:00:10 +0700

    Add Get All Vendors for ADMIN and other roles

--- backend/vendor-service/src/main/java/com/example/vendor_service/repository/VendorRepository.java
+++ backend/vendor-service/src/main/java/com/example/vendor_service/repository/VendorRepository.java
@@ -1,14 +1,17 @@
 package com.example.vendor_service.repository;
 
+import com.example.shared.enums.VendorStatus;
 import com.example.vendor_service.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor,String> , JpaSpecificationExecutor<Vendor> {
     Optional<Vendor> findByName(String name);
     Optional<Vendor> findByManagerId(String managerId);
+    List<Vendor> findAllByStatus(VendorStatus status);
 }
