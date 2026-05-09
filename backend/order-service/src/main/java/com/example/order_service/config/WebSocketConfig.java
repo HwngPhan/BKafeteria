ed00b2316891d5bb6153779d3b287cf657dd727d
Hưng Phan <phanphuochung.07022004@gmail.com>
2026-05-09 22:54:33 +0700

    Hot fix: ws CORS

--- backend/order-service/src/main/java/com/example/order_service/config/WebSocketConfig.java
+++ backend/order-service/src/main/java/com/example/order_service/config/WebSocketConfig.java
@@ -20,9 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOrigins("http://localhost:3000");
     }
 }
