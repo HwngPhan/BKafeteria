7a4d29584cf68f0205b44d3b6dca6cef067bf19e
Hưng Phan <phanphuochung.07022004@gmail.com>
2026-05-10 19:33:01 +0700

    fix bug User profile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ['@aws-sdk/client-s3'],
 };
 
 export default nextConfig;
